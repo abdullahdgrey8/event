e27d22f390acc453cb3f6c3274bbe75cd726d3bf
abdullahdgrey8 <abdullahkhalid204606@gmail.com>
2024-02-15 11:59:27 +0500

    view candidates with styles implemented

--- resources/views/createevent.blade.php
+++ resources/views/createevent.blade.php
@@ -8,8 +8,7 @@
             <div class="col-md-3 left_col">
                 <div class="left_col scroll-view">
                     <div class="navbar nav_title" style="border: 0;">
-                        <!-- <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella
-                                Alela!</span></a> -->
+
                         <img src="{{ asset('/build/images/ttl.svg') }}" alt="">
                     </div>
 
@@ -29,31 +28,7 @@
 
                     <br />
 
-                    <!-- sidebar menu -->
-                    <!-- <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-                        <div class="menu_section">
-                            <h3>General</h3>
-                            <ul class="nav side-menu">
-                                <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ url('/dashboard') }}">Dashboard</a></li>
-                                    </ul>
-                                </li>
-                                <!-- <li><a><i class="fa fa-edit"></i> Forms <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        
-                                    </ul>
-                                </li> -->
-                                <!-- <li><a><i class="fa fa-table"></i> Events <span class="fa fa-chevron-down"></span></a>
-                                    <ul class="nav child_menu">
-                                        <li><a href="{{ url('view-events') }}">View all events</a></li>
-                                        <li><a href="{{ url('add-event') }}">Create an event</a></li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-
-                    </div>  -->
+                    
                     <!-- /sidebar menu -->
                     @include('layouts.sidebar')
 
