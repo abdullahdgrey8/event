bd935c259811ba1b08e080870ef6e86ae25764c6
asfarwaheed01 <asfar1024962@gmail.com>
2024-02-23 11:39:06 +0500

    style changes

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,41 +1,43 @@
-<div class="bg-white">
+<div class="">
     <div class='flex justify-between p-4 top-bar'>
         <div class="ttl-logo"><img src="{{ asset('/build/images/logo.png') }}" alt=""></div>
-            <nav class="nav navbar-nav">
-                        <ul class=" extra-navbar-right">
-                            <li class="nav-item dropdown open" style="padding-left: 15px;">
-                                <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true"
-                                    id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                                    <img src="{{ asset('/build/images/img.jpg') }}" alt="">John Doe
-                                </a>
-                                <div class="dropdown-menu dropdown-usermenu pull-right"
-                                    aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="javascript:;"> Profile</a>
-                                    <a class="dropdown-item" href="javascript:;">
-                                        <span class="badge bg-red pull-right">50%</span>
-                                        <span>Settings</span>
-                                    </a>
-                                    <a class="dropdown-item" href="javascript:;">Help</a>
-                                    <a class="dropdown-item" href="{{ route('get.destroy') }}"><i class="fa fa-sign-out pull-right"></i>
-                                        Log Out</a>
-                                </div>
-                            </li>
-                        </ul>
-            </nav>
-        </div>
+        <nav class="nav navbar-nav">
+            <ul class=" extra-navbar-right">
+                <li class="nav-item dropdown open" style="padding-left: 15px;">
+                    <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown"
+                        data-toggle="dropdown" aria-expanded="false">
+                        <img src="{{ asset('/build/images/img.jpg') }}" alt="">John Doe
+                    </a>
+                    <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="javascript:;"> Profile</a>
+                        <a class="dropdown-item" href="javascript:;">
+                            <span class="badge bg-red pull-right">50%</span>
+                            <span>Settings</span>
+                        </a>
+                        <a class="dropdown-item" href="javascript:;">Help</a>
+                        <a class="dropdown-item" href="{{ route('get.destroy') }}"><i
+                                class="fa fa-sign-out pull-right"></i>
+                            Log Out</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
 
-    <div class="tabs">
-        <ul class="extra-navbar-right tab-design">
+    <div class="tabs bg-white">
+        <ul class="extra-navbar-right tab-design p-0">
             <li class="flex gap-4">
                 <img src="{{ asset('/build/images/calendar.svg') }}" alt="">
-                <a href="{{ url('view-events') }}">View all events</a></li>
+                <a href="{{ url('view-events') }}">View all events</a>
+            </li>
             <li>
-            <img src="{{ asset('/build/images/export.svg') }}" alt="">
-            <a href="{{ url('add-event') }}">Create an event</a></li>
+                <img src="{{ asset('/build/images/export.svg') }}" alt="">
+                <a href="{{ url('add-event') }}">Create an event</a>
+            </li>
         </ul>
     </div>
 
-<!-- <div class="top_nav">
+    <!-- <div class="top_nav">
                 <div class="nav_menu">
                     <div class="nav toggle">
                         <a id="menu_toggle"><i class="fa fa-bars"></i></a>
